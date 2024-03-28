6692875057c391e1a8b30f8e43611fe09cb2fdc9
Michael Maxfield <robmaxfield22@gmail.com>
2024-03-28 14:52:53 -0600

    First commit of js for button functions

---
+++ assets/js/blog.js
@@ -0,0 +1,6 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+localStorage.getItem('blogPost');
---
+++ assets/js/form.js
@@ -0,0 +1,22 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+const userName = document.getElementById('name');
+const postTitle = document.getElementById('postTitle');
+const userPost = document.getElementById('msg');
+const submitButton = document.getElementsByClassName('button');
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const blogPost = {
+        postTitle: postTitle.value,
+        userPost: userPost.value,
+        userName: userName.value,
+    };
+
+    localStorage.setItem('blogPost', JSON.stringify(blogPost));
+
+})
